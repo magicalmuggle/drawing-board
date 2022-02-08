17bffd4fd5430af38d4b788db42853ef9bb371f5
richardhtw <richardhtw@outlook.com>
2022-02-08 10:38:48 +0800

    优化线条绘制方式

--- main.js
+++ main.js
@@ -3,26 +3,38 @@ let canvas = document.getElementById("canvas");
 /** @type {CanvasRenderingContext2D} */
 let ctx = canvas.getContext("2d")
 let painting = false
-let radius = 6
-let isTouchDevice = 'ontouchstart' in document.documentElement
+let isTouchDevice = "ontouchstart" in document.documentElement
+let lastCoordinate
 
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
 
-ctx.fillStyle = "black"
-ctx.strokeStyle = "none"
+ctx.strokeStyle = "black"
+ctx.lineWidth = 6
+ctx.lineCap = "round"
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.stroke()
+}
 
 if (isTouchDevice) {
+  canvas.ontouchstart = (e) => {
+    let touch = e.touches[0]
+    lastCoordinate = [touch.clientX, touch.clientY]
+  }
+
   canvas.ontouchmove = (e) => {
     let touch = e.touches[0]
-    ctx.beginPath()
-    ctx.arc(touch.clientX, touch.clientY, radius, 0, 2 * Math.PI)
-    ctx.stroke()
-    ctx.fill()
+    drawLine(lastCoordinate[0], lastCoordinate[1], touch.clientX, touch.clientY)
+    lastCoordinate = [touch.clientX, touch.clientY]
   }
 } else {
-  canvas.onmousedown = () => {
+  canvas.onmousedown = (e) => {
     painting = true
+    lastCoordinate = [e.clientX, e.clientY]
   }
 
   canvas.onmouseup = () => {
@@ -31,10 +43,8 @@ if (isTouchDevice) {
 
   canvas.onmousemove = (e) => {
     if (painting) {
-      ctx.beginPath()
-      ctx.arc(e.clientX, e.clientY, radius, 0, 2 * Math.PI)
-      ctx.stroke()
-      ctx.fill()
+      drawLine(lastCoordinate[0], lastCoordinate[1], e.clientX, e.clientY)
+      lastCoordinate = [e.clientX, e.clientY]
     }
   }
 }
