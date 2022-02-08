fb501321d668eb0d09b9501df152be00dfd3d67b
richardhtw <richardhtw@outlook.com>
2022-02-08 10:08:36 +0800

    增加手机画图功能

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let canvas = document.getElementById("canvas");
 /** @type {CanvasRenderingContext2D} */
 let ctx = canvas.getContext("2d")
 let painting = false
-radius = 6
+let radius = 6
+let isTouchDevice = 'ontouchstart' in document.documentElement
 
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
@@ -11,48 +12,29 @@ canvas.height = document.documentElement.clientHeight
 ctx.fillStyle = "black"
 ctx.strokeStyle = "none"
 
-canvas.onmousedown = () => {
-  painting = true
-}
-
-canvas.onmouseup = () => {
-  painting = false
-}
-
-canvas.onmousemove = (e) => {
-  if (painting) {
+if (isTouchDevice) {
+  canvas.ontouchmove = (e) => {
+    let touch = e.touches[0]
     ctx.beginPath()
+    ctx.arc(touch.clientX, touch.clientY, radius, 0, 2 * Math.PI)
     ctx.stroke()
-    ctx.arc(e.clientX, e.clientY, radius, 0, 2 * Math.PI)
     ctx.fill()
   }
+} else {
+  canvas.onmousedown = () => {
+    painting = true
+  }
+
+  canvas.onmouseup = () => {
+    painting = false
+  }
+
+  canvas.onmousemove = (e) => {
+    if (painting) {
+      ctx.beginPath()
+      ctx.arc(e.clientX, e.clientY, radius, 0, 2 * Math.PI)
+      ctx.stroke()
+      ctx.fill()
+    }
+  }
 }
-
-
-
-// let ctx = canvas.getContext("2d");
-
-// let painting = false
-
-
-// ctx.fillStyle = "black";
-// ctx.strokeStyle = 'none'
-
-// canvas.onmousedown = () => {
-//   painting = true
-// }
-
-// canvas.onmousemove = (e) => {
-//   if (painting === true) {
-//     ctx.beginPath();
-//     ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI);
-//     ctx.stroke();
-//     ctx.fill();
-//   } else {
-//     console.log('什么都不做')
-//   }
-// }
-
-// canvas.onmouseup = () => {
-//   painting = false
-// }
